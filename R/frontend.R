82c2683016fb493ea9748e1f155807be37b55fcc
Juha Itkonen <itkonen.juha@gmail.com>
2021-03-13 08:11:45 +0200

    Add user agent

--- R/frontend.R
+++ R/frontend.R
@@ -57,6 +57,8 @@ data <- function(pattern = "", dl_filter = NULL, labels = TRUE,
   do_request("data", c(as.list(environment()), list(...)))
 }
 
+user_agent <-
+  httr::user_agent(paste0("R/robonomistClient"/, packageVersion("robonomistClient")))
 
 do_request <- function(fun, args) {
   cli::cli_process_start("Processing request", on_exit = "done"); on.exit(cli::cli_status_clear())
@@ -74,6 +76,7 @@ do_request <- function(fun, args) {
       paste0(getOption("robonomist.protocol"), "://", getOption("robonomist.server"), "/data_remote"),
       body = serialize(payload, NULL),
       encode = "raw",
+      user_agent,
       httr::content_type("application/octet-stream"),
       httr::timeout(60L*1000L))
     if(httr::http_error(req)) {
@@ -83,3 +86,4 @@ do_request <- function(fun, args) {
     unserialize(httr::content(req))
   }
 }
+
