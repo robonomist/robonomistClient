784cd22a77bb1581473efd61ce9c568bbdb4415c
Juha Itkonen <itkonen.juha@gmail.com>
2021-03-24 16:11:14 +0200

    add tbl_lazy_api

---
+++ R/tbl_lazy.oecd.R
@@ -0,0 +1,107 @@
+#' @import dplyr
+#' @export
+print.tbl_lazy_oecd <- function(x, n = 10L, ...) {
+
+  type <- c("code", "label")[x$labels+1]
+
+  categories_n <- function(ns) {
+    rns <- rev(ns)
+    sel <- rep(1, length(ns))
+    for(i in seq_along(rns)) {
+      p <- prod(sel)
+      if(p < n) {
+        remaining  <- n %/% p + 1L
+        sel[i] <- min(remaining, rns[i])
+      }
+    }
+    rev(sel)
+  }
+
+  if("Frequency" %in% names(x$x)) {
+    ## Filter out incompatible dates
+    time_cols <-
+      inner_join(x$x$Frequency,
+                 select(x$x$time, date_type, time),
+                 by = c("code" = "date_type")) %>%
+      select(Frequency = type, time) %>%
+      arrange(time)
+    not_time <- setdiff(names(x$x), c("Frequency", "time"))
+    sel <- categories_n(
+      c(x$ns[not_time], nrow(time_cols)))
+    dims <- map2(x$x[not_time], sel[1:length(not_time)], ~.x[1:.y,][[type]])
+    c(dims, time_cols)
+    r <- crossing(expand_grid(!!!dims), time_cols)
+  } else {
+    sel <- categories_n(x$ns)
+    types <- case_when(names(x$x) == "time" ~ "time", TRUE ~ type)
+    dims <- pmap(list(.x = x$x, .y = sel, .z = types),
+                 function(.x, .y, .z) .x[1:.y, ][[.z]])
+    r <- expand_grid(!!!dims)
+  }
+  r <- mutate(r, value = vctrs::new_vctr("??", class = "collect"))
+  attr(r, "n_rows") <- prod(map_int(x$x, nrow))
+  attr(r, "title") <- x$title
+  attr(r, "robonomist_id") <- attr(x, "robonomist_id")
+  class(r) <- c("robonomist_data", "lazy_oecd_printout", class(r))
+  print(r, n = n)
+  invisible(x)
+}
+
+#' @export
+tbl_sum.lazy_oecd_printout <- function(x, ...) {
+  c("OECD" = "Uncollected data structure",
+    "Title" = attr(x, "title"))
+}
+
+#' @importFrom pillar tbl_format_footer style_subtle
+#' @export
+tbl_format_footer.lazy_oecd_printout <- function(x, setup, ...) {
+  default_footer <- NextMethod()
+  extra_info <-
+    "# This data has not yet been collected from OECD api, and all rows might not be available. Please use `filter` to limit the number of rows under a million,  and use `collect` to retrieve actual data."
+  c(default_footer, extra_info)
+}
+
+#' @export
+dim.lazy_oecd_printout <- function(x) {
+  c(attr(x, "n_rows"), length(x))
+}
+
+
+#' @export
+filter.tbl_lazy_oecd <- function(.data, ..., .preserve = FALSE) {
+  if (!identical(.preserve, FALSE)) {
+    stop("`.preserve` is not supported on database backends", call. = FALSE)
+  }
+ #browser()
+  dots <- quos(...)
+  vars <- .data$vars
+
+  getAST <- function(x) purrr::map_if(as.list(x), is.call, getAST)
+  symbols <- map(rlang::exprs(...), ~as.character(keep(unlist(getAST(.x)), is.symbol)))
+  filtered_vars <-
+    map_chr(symbols, function(x) {
+      i <- which(map_lgl(vars, ~any(x == .x)))
+      if(length(i) != 1)
+        stop("Filtering expressions must contain exactly one table variable.", call.=F)
+      vars[i]
+    })
+
+  type <- c("code", "label")[.data$labels+1]
+  type <- case_when(filtered_vars == "time" ~ "time", TRUE ~ type)
+  for(i in seq_along(filtered_vars)) {
+    keep_rows <- rlang::eval_tidy(dots[[i]], map(.data$x, type[i]))
+    .data$x[[filtered_vars[i]]] <- .data$x[[filtered_vars[i]]][keep_rows, ]
+  }
+  .data
+}
+
+#' @export
+#' @importFrom tibble tbl_sum
+tbl_sum.robonomist_oecd_data <- function(x, ...) {
+  header <- NextMethod()
+  y <- attr(x, "oecd_title", exact = FALSE)
+  if (!is.null(y))
+    header <- c(header, Title = unname(y))
+}
+
---
+++ R/tbl_lazy_api.R
@@ -0,0 +1,99 @@
+#' @export
+vec_ptype_abbr.collect <- function(x) {
+  "collect"
+}
+
+#' @importFrom pillar pillar_shaft
+#' @export
+pillar_shaft.collect <- function(x, ...) {
+  out <- format(x)
+  pillar::new_pillar_shaft_simple(out, align = "right", min_width = 9)
+}
+
+#' @export
+dimnames.tbl_lazy_api <- function(x) {
+  list(NULL, x$vars)
+}
+
+#' @export
+dim.tbl_lazy_api <- function(x) {
+  c(prod(unlist(lapply(x$x, nrow), use.names = FALSE)), length(x$ns))
+}
+
+
+#' @export
+distinct.tbl_lazy_api <- function(.data, ..., .keep_all = FALSE) {
+  if (rlang::dots_n(...) > 0) {
+    if (.keep_all) {
+      stop(
+        "Can only find distinct value of specified columns if .keep_all is FALSE",
+        call. = FALSE
+      )
+    }
+    dots <- ensyms(...)
+    with(expand_grid(!!!dots), data = map(.data$x, "label"))
+  } else {
+    .data
+  }
+}
+
+
+#' @export
+collect.tbl_lazy_api <- function(x, ...) {
+  data_get(x$src, dl_filter = x)
+}
+
+#' @export
+as.data.frame.tbl_lazy_api <- function(x, ...) {
+  dplyr::collect(x)
+}
+
+#' @export
+select.tbl_lazy_api <- function(.data, ...) {
+  dplyr::select(collect(.data), ...)
+}
+
+#' @export
+relocate.tbl_lazy_api <- function(.data, ...) {
+  dplyr::relocate(collect(.data), ...)
+}
+
+#' @export
+rename.tbl_lazy_api <- function(.data, ...) {
+  dplyr::rename(collect(.data), ...)
+}
+
+#' @export
+summarise.tbl_lazy_api <- function(.data, ...) {
+  dplyr::select(collect(.data), ...)
+}
+
+#' @export
+group_by.tbl_lazy_api <- function(.data, ...) {
+  dplyr::group_by(collect(.data), ...)
+}
+
+#' @export
+transmute.tbl_lazy_api <- function(.data, ...) {
+  dplyr::transmute(collect(.data), ...)
+}
+
+#' @export
+mutate.tbl_lazy_api <- function(.data, ...) {
+  dplyr::mutate(collect(.data), ...)
+}
+
+#' @export
+arrange.tbl_lazy_api <- function(.data, ...) {
+  dplyr::arrange(collect(.data), ...)
+}
+
+## #' @export
+## names.tbl_lazy_api <- function(x) x$vars
+
+#' @export
+`names<-.tbl_lazy_api` <- function(x, value) {
+  y <- collect(x)
+  `names<-`(y, c(value, names(y)[-(1:length(value))]))
+}
+
