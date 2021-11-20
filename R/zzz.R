59303c622c1b6e7ad195f4311f3235b1b0eb14a5
Juha Itkonen <itkonen.juha@gmail.com>
2021-11-20 06:27:21 +0200

    Fix startup message

--- R/zzz.R
+++ R/zzz.R
@@ -4,12 +4,15 @@
 
   hostname <- Sys.getenv("ROBONOMIST_SERVER")
   access_token <- Sys.getenv("ROBONOMIST_ACCESS_TOKEN")
+
   if (nzchar(hostname)) {
     set_robonomist_server(hostname, access_token)
   } else if ("robonomistServer" %in% installed.packages()) {
-    cli::cli_alert_success("Using local {.pkg robonomistServer} {utils::packageVersion('robonomistServer')}")
+    cli::cli_alert_success(
+      "Using local {.pkg robonomistServer} {utils::packageVersion('robonomistServer')}"
+    )
   } else if (interactive()){
-    connection$please_set_server()
+    please_set_server()
   }
 
   invisible(NULL)
