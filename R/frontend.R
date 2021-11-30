ec25d9ba50285482c5110fc86e24caa99a8f3f49
Juha Itkonen <itkonen.juha@gmail.com>
2021-11-30 11:19:12 +0200

    Document

--- R/frontend.R
+++ R/frontend.R
@@ -9,8 +9,12 @@ data_vintage <- function(id) {
 
 #' Metadata of table
 #'
-#' Currently works only with px-based tables
-#' @param String, exact id of table
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' The output of this function is likely to change. Currently works only with px-based tables.
+#'
+#' @param id, Exact table id
 #' @return List of metadata
 #'
 #' @export
@@ -20,7 +24,9 @@ data_metadata <- function(id) {
 
 #' Get data for table id
 #'
-#' @param id string. Table id
+#' Returns the objects without performing searching or pattern filters. Fails if no match exists.
+#'
+#' @param id string. Exact table id
 #' @export
 data_get <- function(id, dl_filter = NULL, labels = TRUE, lang = NULL, na.rm = FALSE, ...) {
   do_request("get", c(as.list(environment()), list(...)))
@@ -45,10 +51,10 @@ datasources <- function() {
 #'
 #' @description Convenience api that searches when multiple table id matches are found and filters the table using the section pattern
 #'
-#' @param pattern string. A sequence of regular expressions separated by section sign \code{§}, which are used to filter the database. The first expression in the sequence is matched to a table id. The following expressions match to table variables in sequence. If the last variable is a date, it is used as start date filter.
+#' @param pattern string, A sequence of regular expressions separated by section sign \code{§}, which are used to filter the database. The first expression in the sequence is matched to a table id. The following expressions match to table variables in sequence. If the last variable is a date, it is used as start date filter.
 #' @param dl_filter list or named vector passed to datasource download functions for filtering incoming data. Supported by tulli, OECD and ECB.
 #' @param labels logical. Some datasources can return labelled or coded data.
-#' @param lang Still experimental
+#' @param lang Two-letter language code, e.g. "en" or "sv".
 #' @param na.rm Px-file based datasources return a table with a combination of all categories. Missing values can be filtered when reading the file to improve preformance.
 #'
 #' @export
