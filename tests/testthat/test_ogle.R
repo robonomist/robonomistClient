dc38cbe251d851a55fb065b7dfc02d0028e0d52a
Juha Itkonen <itkonen.juha@gmail.com>
2022-05-10 20:09:37 +0300

    Fix ids

--- tests/testthat/test_ogle.R
+++ tests/testthat/test_ogle.R
@@ -1,12 +1,12 @@
 
 
 test_that("ogle() works", {
-  d <- data("StatFin/asu/asas/statfin_asas_pxt_115a.px")
+  d <- data("StatFin/asas/statfin_asas_pxt_115a.px")
   expect_type(ogle(d), "list")
 })
 
 test_that("print_filter() works", {
-  d <- data("StatFin/asu/asas/statfin_asas_pxt_115a.px")
+  d <- data("StatFin/asas/statfin_asas_pxt_115a.px")
   expect_type(print_filter(d), "list")
 })
 
