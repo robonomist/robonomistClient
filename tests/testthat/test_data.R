48ee5bbc6e45e0423dc06c8fabea12aec2cd2a60
Juha Itkonen <itkonen.juha@gmail.com>
2022-08-25 14:47:00 +0300

    Fix test

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -5,11 +5,11 @@ for(i in ds$dataset) {
   test_that(paste("data() works on datasource", i), {
     skip_if(getOption("robonomist.skip.server.test", FALSE), "Test server not configured.")
     n <- switch(i, konj = 35, dk = 2, no = 2, traficom = 4, tidy = 2,
-                se = 10, unctad = 2, eia = 16, 1)
+                se = 10, unctad = 2, eia = 16, tutkihallintoa = 2, 1)
     if(i %in% c("tulli", "ecb", "bundesbank")) {
-      expect_s3_class(data(paste0(i, "/#", n)), "list")
+      expect_s3_class(data(paste0(i, "/#", n)), c("robonomist_data", "list"))
     } else {
-      expect_s3_class(data(paste0(i, "/#", n)), "tbl")
+      expect_s3_class(data(paste0(i, "/#", n)), c("robonomist_data", "tbl"))
     }
   })
 }
