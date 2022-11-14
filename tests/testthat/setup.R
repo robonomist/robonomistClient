54a39061873edf04a5ff97ad09c1542a83420e74
Juha Itkonen <itkonen.juha@gmail.com>
2022-11-14 15:40:23 +0200

    Fix test

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,17 +1,19 @@
 start_test_server <- function(port) {
   callr::r_bg(function(port) {
-    robonomistServer::data_server(port = port, access_token = "abc")
+    robonomistServer::data_server(port = port, workers = 1)
   }, args = list(port = port))
 }
 
-if(isTRUE(getOption("robonomist.test.local"))) {
+if (isTRUE(getOption("robonomist.test.local"))) {
+  message("Testing locally")
   set_robonomist_server(NULL)
-} else if(!is.null(getOption("robonomist.test.server"))) {
+} else if (!is.null(getOption("robonomist.test.server"))) {
   set_robonomist_server(
     host = getOption("robonomist.test.server"),
     access_token = getOption("robonomist.test.access_token", "")
   )
 } else if (requireNamespace("robonomistServer")) {
+  message("Starting test server...")
   port <- parallelly::freePort()
   rb <- start_test_server(port)
   withr::defer(rb$kill(), teardown_env())
@@ -19,7 +21,7 @@ if(isTRUE(getOption("robonomist.test.local"))) {
     host = paste0("127.0.0.1:", port),
     access_token = "abc"
   )
-  Sys.sleep(30)
+  Sys.sleep(3)
 } else {
   op_skip_test <- options(robonomist.skip.server.test = TRUE)
   withr::defer(options(robonomist.skip.server.test = op_skip_test), teardown_env())
--- tests/testthat/test_echo_devel.R
+++ tests/testthat/test_echo_devel.R
@@ -1,6 +1,6 @@
 test_that("Conditions work", {
   skip_if(getOption("robonomist.skip.server.test", FALSE), "Test server not configured.")
   expect_message(data("tidy/devel_echo", args = "cli_alert", hash = Sys.time()))
-  expect_warning(data("tidy/devel_echo", args = "warning", hash = Sys.time()))
+  expect_message(data("tidy/devel_echo", args = "warning", hash = Sys.time()))
   expect_error(data("tidy/devel_echo", args = "error", hash = Sys.time()))
 })
